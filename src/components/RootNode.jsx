4a118d10ffca831c5b7cc4014df7b54e0a880b38
Timo Sutterer <hi@timo-sutterer.de>
2022-01-24 14:17:45 +0100

    fix: use correct handle type for root node

--- src/components/RootNode.jsx
+++ src/components/RootNode.jsx
@@ -2,12 +2,12 @@ import React, { memo } from "react";
 import { Handle } from "react-flow-renderer";
 import onDoubleClickNode from "./NodeInteractions";
 
-const RootNode = ({ id, data, isConnectable, sourcePosition = "left" }) => {
+const RootNode = ({ id, data, isConnectable, targetPosition = "left" }) => {
     return (
         <div className="react-flow__node-output" onDoubleClick={(evt) => onDoubleClickNode(evt, id)}>
             <Handle
-                type="source"
-                position={sourcePosition}
+                type="target"
+                position={targetPosition}
                 isConnectable={isConnectable}
             />
             {data.label}

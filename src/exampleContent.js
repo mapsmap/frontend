96e3af0279a564a9ea9ce90eac7c80e6a3de745b
Timo Sutterer <hi@timo-sutterer.de>
2022-02-21 13:35:09 +0100

    refactor: strip whitespace from example text

--- src/exampleContent.js
+++ src/exampleContent.js
@@ -1,21 +1,19 @@
 export const exampleTitle = "New mind substrate";
 
-export const exampleText = `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Aenean non nisl quis felis posuere finibus.
-    In varius aliquet rutrum.
-    Etiam mattis massa nec mauris posuere auctor.
-    In at porttitor mi.
-    Nulla ultricies magna id metus sollicitudin, eget cursus odio gravida.
-    Vivamus tristique consectetur neque sodales aliquam.
-    Proin laoreet scelerisque placerat.
-    Proin quis facilisis sem.
-    Maecenas accumsan sed erat at lacinia.
-    Phasellus erat purus, luctus sit amet orci ac, maximus sollicitudin mauris.
-    Phasellus gravida quam et nisl eleifend eleifend.
-    Fusce egestas nisl euismod bibendum efficitur.
-    Vestibulum in tristique velit, sed efficitur lacus.
-`;
+export const exampleText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Aenean non nisl quis felis posuere finibus.
+In varius aliquet rutrum.
+Etiam mattis massa nec mauris posuere auctor.
+In at porttitor mi.
+Nulla ultricies magna id metus sollicitudin, eget cursus odio gravida.
+Vivamus tristique consectetur neque sodales aliquam.
+Proin laoreet scelerisque placerat.
+Proin quis facilisis sem.
+Maecenas accumsan sed erat at lacinia.
+Phasellus erat purus, luctus sit amet orci ac, maximus sollicitudin mauris.
+Phasellus gravida quam et nisl eleifend eleifend.
+Fusce egestas nisl euismod bibendum efficitur.
+Vestibulum in tristique velit, sed efficitur lacus.`;
 
 export const exampleSidebar = {
     relatedNodes: [

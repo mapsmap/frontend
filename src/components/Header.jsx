4abb493bccc6a8e53e4f6bf5119c9bad7c07f646
Timo Sutterer <hi@timo-sutterer.de>
2022-02-23 10:43:12 +0100

    fix: remove unused import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';

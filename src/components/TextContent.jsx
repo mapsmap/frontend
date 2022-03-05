a8d332b598365120a24779efd2116a25972f3cc1
Timo Sutterer <hi@timo-sutterer.de>
2022-03-05 14:23:56 +0100

    fix: don't open import modal

--- src/components/TextContent.jsx
+++ src/components/TextContent.jsx
@@ -1,17 +1,11 @@
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
-import { PickerOverlay } from 'filestack-react';
 
 export default function TextContent(props) {
     const { text } = props;
 
     return (
         <Grid item xs={12} md={8}>
-            <PickerOverlay
-                apikey="ArlTDWwZTsCWKOl4lISx2z"
-                onSuccess={(res) => console.log(res)}
-                onUploadDone={(res) => console.log(res)}
-            />
             <Divider />
             <div>{text}</div>
         </Grid>
